254f0c811aa04112ab6c48cabf05d3d1b38a817d
super1peng <super1peng.gmail.com>
2019-02-22 16:44:04 +0800

    Initial commit

---
+++ 02_Java_Language/src/com/super1peng/demo/Hello.java
@@ -0,0 +1,12 @@
+package com.super1peng.demo;
+
+/**
+ * @Author: super1peng
+ * @Date: 2019/2/22 下午4:35
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("ceshi");
+    }
+}
